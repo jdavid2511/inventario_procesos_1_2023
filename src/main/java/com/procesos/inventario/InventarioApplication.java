47bbe27eb958e19d50a34e5605c2235ddd4ba937
David garcia <jdgarciat@ufpso.edu.co>
2023-03-07 21:15:02 -0500

    agregar servicios y endpoin de usuario

--- src/main/java/com/procesos/inventario/InventarioApplication.java
+++ src/main/java/com/procesos/inventario/InventarioApplication.java
@@ -1,10 +1,12 @@
 package com.procesos.inventario;
 
+import lombok.Data;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@Data
+@SpringBootApplication
 public class InventarioApplication {
 
 	public static void main(String[] args) {
@@ -12,3 +14,4 @@ public class InventarioApplication {
 	}
 
 }
+
--- src/main/java/com/procesos/inventario/models/User.java
+++ src/main/java/com/procesos/inventario/models/User.java
@@ -2,15 +2,22 @@ package com.procesos.inventario.models;
 
 import lombok.Data;
 
+import javax.persistence.*;
 import java.util.Date;
 
 @Data
+@Entity
+
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String FirstName;
-    private String LastName;
-    private String Address;
-    private String Email;
-    private String Password;
-    private Date Birthday;
+    @Column(name = "first_name")
+    private String firstName;
+    @Column(name = "last_name")
+    private String lastName;
+    private String address;
+    private String email;
+    private String password;
+    private Date birthday;
 }
